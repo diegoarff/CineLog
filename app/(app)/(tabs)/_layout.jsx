97cbe26ba747c0906497e2ccaf746ce22ed1b543
diegoarff <diegorinconf1@gmail.com>
2023-12-06 21:56:56 -0400

    style: change tab bar styles

--- app/(app)/(tabs)/_layout.jsx
+++ app/(app)/(tabs)/_layout.jsx
@@ -1,11 +1,75 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
+const colors = require("tailwindcss/colors");
 
 const TabLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen name="home" options={{ headerShown: false }} />
-      <Tabs.Screen name="search" />
-      <Tabs.Screen name="chat" />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.teal[500],
+        tabBarStyle: {
+          backgroundColor: "#101015",
+          borderTopWidth: 0,
+          height: 70,
+        },
+        tabBarLabelStyle: {
+          fontSize: 14,
+          fontFamily: "interSemiBold",
+          marginBottom: 10,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+          tabBarItemStyle: {
+            gap: -10,
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Discover",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "compass" : "compass-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+          tabBarItemStyle: {
+            gap: -10,
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          headerShown: false,
+          tabBarLabel: "Chat",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? "chatbubble" : "chatbubble-outline"}
+              size={size}
+              color={color}
+            />
+          ),
+          tabBarItemStyle: {
+            gap: -10,
+          },
+        }}
+      />
     </Tabs>
   );
 };
